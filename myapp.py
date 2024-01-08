6f4f1b2d91ed2d09a2772df9d5504221957b01a4
syam-kummari <147791228+syam-kummari@users.noreply.github.com>
2024-01-08 12:00:54 +0530

    Create myapp.py

---
+++ myapp.py
@@ -0,0 +1,13 @@
+import streamlit as st
+st.set_page_config(page_title = "TOPG")
+st.header("Andrew tate")
+
+col1,col2 = st.columns(2)
+with col1:
+  st.subheader("TOPG")
+  st.image("1678030997253 (1)-02.jpg")
+  st.write("What colour is your bugatti")
+with col2:
+  st.subheader("Tate")
+  st.image("1704123703990-01.webp")
+  st.write("Escape the matrix")
